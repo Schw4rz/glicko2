81bf076746dc4a8a21ac98fda3f20b7d335b5957
Schw4rz <schwarz.sebastian.ss@googlemail.com>
2018-01-22 21:21:25 +0100

    added basic test

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(glicko2)
+
+test_check("glicko2")
---
+++ tests/testthat/test_glicko2functions.R
@@ -0,0 +1,11 @@
+context("Glicko-2 Functions")
+
+test_that("Glicko-2 update is correct", {
+  expect_equal(GetGlicko2Rating(1500, 200, 0.06,
+                                c(1400, 1550, 1700),
+                                c(30, 100, 300),
+                                c(1, 0, 0), 0.5, 1e-6),
+               list(r.new = 1464.050671,
+                    RD.new = 151.5165241,
+                    sigma.new = 0.05999598429))
+})
